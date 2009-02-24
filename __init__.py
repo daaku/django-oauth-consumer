072766800016ed9483a34fad2060f57ada062835
Naitik Shah <naitik.shah@yahoo.com>
2009-02-23 21:10:02 -0800

    more cleanup

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import urllib, urlparse, cgi, logging
 from functools import wraps
+import urlencoding
 import oauth
 import collections
 from make_request import make_request
@@ -23,7 +24,10 @@ class OAuthConsumerApp(object):
 
     def __init__(self, config):
         self.name = config['name']
-        self.consumer = {'oauth_token': config.get('consumer_key'), 'oauth_token_secret': config.get('consumer_secret')}
+        self.consumer = {
+            'oauth_token': config.get('consumer_key'),
+            'oauth_token_secret': config.get('consumer_secret'),
+        }
         self.request_token_url = config.get('request_token_url')
         self.authorization_url = config.get('authorization_url')
         self.access_token_url = config.get('access_token_url')
@@ -58,7 +62,11 @@ class OAuthConsumerApp(object):
         Identical to the make_request API, and accepts an additional (optional)
         token parameter and request object (required if dealing with Scalable
         OAuth service providers). It adds the OAuth Authorization header based
-        on the consumer set on this instance.
+        on the consumer set on this instance. If content not a Mapping object,
+        it will be ignored with respect to signing. This means you need to
+        either pass the query parameters as a dict/Mapping object, or pass a
+        encoded query string as part of the URL which will be extracted and
+        included in the signature.
 
         """
 
@@ -72,6 +80,10 @@ class OAuthConsumerApp(object):
         headers['Authorization'] = orequest.to_header(self.realm)
         response = make_request(url, method=method, content=content, headers=headers)
 
+        # check if we got a scalable oauth token_expired error
+        # we will fetch a new access token using the oauth_session_handle in
+        # the current token and redo the request in this case.
+        # FIXME: infinite loop
         www_auth = response.getheader('www-authenticate', None)
         if www_auth and response.status == 401 and 'token_expired' in www_auth:
             response = self.make_signed_req(
@@ -81,8 +93,8 @@ class OAuthConsumerApp(object):
                 request=request
             )
             body = unicode(response.read(), 'utf8').strip()
-            new_token = oauth.parse_qs(body)
-            request.session[self.name + '_access_token'] = new_token
+            new_token = urlencoding.parse_qs(body)
+            request.session[self.ACCESS_TOKEN_NAME] = new_token
 
             self.got_access_token.send(
                 sender=self,
@@ -96,20 +108,17 @@ class OAuthConsumerApp(object):
             return response
 
     def is_valid_signature(self, request):
-        # create a new dict with the Authorization key that
-        # OAuthRequest.from_request expects.
-        if 'HTTP_AUTHORIZATION' in request.META:
-            headers = {'Authorization': request.META['HTTP_AUTHORIZATION']}
-        else:
-            headers = {}
+        """
+        Validates the incoming *2 legged* signed request. This is useful for
+        signed requests from OpenSocial Containers such as YAP.
 
+        """
         oauth_request = oauth.OAuthRequest(
             request.build_absolute_uri(request.path),
             request.method,
             dict(request.REQUEST.items()),
-            headers,
+            {'Authorization': request.META.get('HTTP_AUTHORIZATION', '')},
         )
-
         oauth_request.validate_signature(self.sig_method, self.consumer)
 
     def validate_signature(self, view):
@@ -147,6 +156,9 @@ class OAuthConsumerApp(object):
 
     def need_authorization(self, request):
         """
+        Fetches a request token from the service provider and renders the
+        need_authorization template.
+
         Renders
             "django_oauth_consumer/{NAME}/need_authorization.html"
 
@@ -154,11 +166,12 @@ class OAuthConsumerApp(object):
 
         response = self.make_signed_req(self.request_token_url)
         body = unicode(response.read(), 'utf8').strip()
-        request_token = oauth.parse_qs(body)
+        request_token = urlencoding.parse_qs(body)
         request.session[self.name + '_request_token'] = request_token
-        qs = urllib.urlencode({
+        qs = urlencoding.compose_qs({
             'oauth_token': request_token['oauth_token'],
-            'oauth_callback': request.build_absolute_uri(reverse(self.SUCCESS_VIEW_NAME, kwargs={'oauth_token': request_token['oauth_token']})),
+            'oauth_callback': request.build_absolute_uri(
+                reverse(self.SUCCESS_VIEW_NAME, kwargs={'oauth_token': request_token['oauth_token']})),
         })
         url = self.authorization_url
         if '?' in url:
@@ -181,7 +194,7 @@ class OAuthConsumerApp(object):
             logging.error('request token in session and url dont match')
         response = self.make_signed_req(self.access_token_url, token=request_token)
         body = unicode(response.read(), 'utf8').strip()
-        access_token = oauth.parse_qs(body)
+        access_token = urlencoding.parse_qs(body)
         request.session[self.ACCESS_TOKEN_NAME] = access_token
         del request.session[self.REQUEST_TOKEN_NAME]
 
